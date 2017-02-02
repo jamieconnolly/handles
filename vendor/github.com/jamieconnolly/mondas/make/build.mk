248bea530ba68a5b3a2b9cdea262e16564b5db89
Jamie Connolly <jamie@jamieconnolly.com>
2017-02-02 17:01:12 +0000

    :arrow_up: Upgrade Mondas

--- vendor/github.com/jamieconnolly/mondas/make/build.mk
+++ vendor/github.com/jamieconnolly/mondas/make/build.mk
@@ -4,6 +4,8 @@ GOOS ?= $(shell go env GOOS)
 LDFLAGS ?= -X main.Name=$(NAME) -X main.Version=$(VERSION)
 GOFLAGS ?= -ldflags "$(LDFLAGS)"
 
+SELF_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+
 bin/$(NAME):
 	@echo "==> Building $@…"
 	@mkdir -p $(@D)
@@ -15,5 +17,5 @@ build: clean bin/$(NAME) completions/$(NAME).zsh
 completions/$(NAME).%:
 	@echo "==> Generating $@…"
 	@mkdir -p $(@D)
-	@cp $(dir $(realpath $(lastword $(MAKEFILE_LIST))))../completions/mondas.$* $@
-	@sed -i "" -e "s/mondas/$(NAME)/g" $@
+	@cp $(SELF_DIR)../completions/mondas.$* "$@"
+	@sed -e "s/mondas/$(NAME)/g" "$@" | tee "$@" >/dev/null
