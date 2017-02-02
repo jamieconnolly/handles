a586b619a5da7bcd125e9abbde9295da9e3643d5
Jamie Connolly <jamie@jamieconnolly.com>
2017-02-02 15:49:48 +0000

    :wrench: Fix .gitignore so it allows the zsh completions template

---
+++ vendor/github.com/jamieconnolly/mondas/completions/mondas.zsh
@@ -0,0 +1,22 @@
+#compdef mondas
+
+local args cmds line ret=1 state
+
+_arguments -C \
+  '--help[Display help information]' \
+  '--version[Display version information]' \
+  '1: :->cmds' \
+  '*: :->args' && ret=0
+
+case $state in
+  args)
+    args=(${(f)"$(_call_program arguments ${service} completions ${line[@]:0:$((CURRENT-1))} 2>/dev/null)"})
+    _values "${service} arguments" $args && ret=0
+    ;;
+  cmds)
+    cmds=(${(f)"$(_call_program commands ${service} completions 2>/dev/null)"})
+    _describe -t commands "${service} commands" cmds && ret=0
+    ;;
+esac
+
+return ret
